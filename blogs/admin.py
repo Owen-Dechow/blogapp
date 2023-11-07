18fe9e7a6c8dd8283773c417c12e73b104e840ec
Owen Dechow <owen,dechow@gmail.com>
2023-11-06 21:54:45 -0500

    Update Models

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Blog)
+admin.site.register(models.BlogManager)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
+admin.site.register(models.CommentFlag)
+admin.site.register(models.Subscription)
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(to="BlogManager")
+
+
+class BlogManager(models.Model):
+    user = models.ForeignKey(to="User")
+    blog = models.ForeignKey(to="Blog")
+
+
+class Post(models.Model):
+    manager = models.ForeignKey(to="BlogManager")
+    blog = models.ForeignKey(to="Blog")
+    content = models.CharField(max_length=100)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(to="User")
+    blog = models.ForeignKey(to="Blog")
+    parent = models.ForeignKey(to="Comment", null=True)
+
+
+class CommentFlag(models.Model):
+    comment = models.ForeignKey(to="Comment")
+    manager = models.ForeignKey(to="BlogManager")
+    reason = models.CharField(max_length=100)
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(to="User")
+    blog = models.ForeignKey(to="Blog")
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(to="User")
+    content = models.CharField(max_length=100)
